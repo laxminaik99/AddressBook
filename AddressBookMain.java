b58a4900c8e4c6e4a35c886dd930cbb0130ca541
laxminaik99 <lbnaik99@gmail.com>
2024-02-12 13:07:30 +0530

    AddressBookMain

---
+++ AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz8;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program in Address Book Program");
+
+	}
+
+}
